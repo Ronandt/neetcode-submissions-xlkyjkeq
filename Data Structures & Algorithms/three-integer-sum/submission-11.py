cbdbfacc1efe9b6f8627524366ab3ebcfa880d1a
Isaac <49837565+Ronandt@users.noreply.github.com>
2026-04-28 11:51:15 +0800

    Add: three-integer-sum - submission-11

---
+++ Data Structures & Algorithms/three-integer-sum/submission-11.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i, j in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]: #also need to handle for the third pointer 
+                    continue
+            first_pointer = i + 1
+            second_pointer = len(nums) -1
+          
+            while second_pointer > first_pointer:
+
+                res = j + nums[first_pointer] + nums[second_pointer]
+
+                if res == 0: #do it after
+                    result.append([j, nums[first_pointer], nums[second_pointer]]) 
+                    while first_pointer + 1 < len(nums) and (nums[first_pointer + 1] == nums[first_pointer]):
+                        first_pointer += 1
+
+                    while second_pointer -1 >= i+1 and nums[second_pointer -1] == nums[second_pointer]:
+                        second_pointer -= 1
+                    
+                    second_pointer -=1
+                    first_pointer += 1
+
+
+                if res > 0:
+                    second_pointer -=1
+                if res < 0:
+                    first_pointer += 1
+
+        return result
+
+
+                    
